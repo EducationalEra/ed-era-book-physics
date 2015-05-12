6f4e27725201fa98b4fbab316fbd12d82de32c3e
mimorta <gtuk@icloud.com>
2015-05-12 15:57:30 +0300

    ss

--- custom/js/quiz.js
+++ custom/js/quiz.js
@@ -23,7 +23,6 @@ function resetStyle(currentPos) {
     questions[currentPos].getElementsByTagName("explain")[0].setAttribute("style", "display: none");
     var answers = questions[currentPos].getElementsByTagName("choice");
     for (i = 0; i < answers.length; i++) {
-           answers[i].style.color = "black";
            answers[i].getElementsByTagName("m")[0].setAttribute("style", "display: none");     
 }
 }
@@ -39,45 +38,32 @@ function totalCorrect(currentPos) {
     return totalCorrect;
 }
 
-function showAns(currentPos) {
-    resetStyle(currentPos);
-    questions[currentPos].getElementsByTagName("explain")[0].setAttribute("style", "display");
-    var answers = questions[currentPos].getElementsByTagName("choice");
-    for (i = 0; i < answers.length; i++) {
-         questions[currentPos].getElementsByTagName("input")[i].checked = false;
-        if (answers[i].getAttribute("correct") == "true"){
-            answers[i].getElementsByTagName("m")[0].setAttribute("style", "display");
-           answers[i].getElementsByTagName("m")[0].innerHTML = " &#10004";
-           answers[i].style.color = "green";
-        }
-    }
-    
-}
-
 function findCorrect(currentPos) {
     var answers = questions[currentPos].getElementsByTagName("choice"),
         inputs = questions[currentPos].getElementsByTagName("input"),
         corAns = 0, incorAns = 0;
     resetStyle(currentPos);
     for (i = 0; i < answers.length; i++) {
-        if (answers[i].getElementsByTagName("input")[0].checked == true && answers[i].getAttribute("correct") == "true") {
+        
+        if (answers[i].getAttribute("correct") == "true"){
+            answers[i].getElementsByTagName("m")[0].setAttribute("style", "display");
+           answers[i].getElementsByTagName("m")[0].innerHTML = " &#10004";
+            answers[i].getElementsByTagName("m")[0].style.color="green";
+        }
+        
+        
+        else if (answers[i].getElementsByTagName("input")[0].checked == true && answers[i].getAttribute("correct") == "true") {
            corAns++;
            answers[i].getElementsByTagName("m")[0].setAttribute("style", "display");
            answers[i].getElementsByTagName("m")[0].innerHTML = " &#10004";
-           answers[i].style.color = "green";
-            
+            answers[i].getElementsByTagName("m")[0].style.color="green";
         }
         else if (answers[i].getElementsByTagName("input")[0].checked == true && answers[i].getAttribute("correct") == "false") {
             incorAns++;
             answers[i].getElementsByTagName("m")[0].setAttribute("style", "display");
             answers[i].getElementsByTagName("m")[0].innerHTML = " &#10008";
-            answers[i].style.color="red";
+            answers[i].getElementsByTagName("m")[0].style.color="red";
             }
-         if (answers[i].getAttribute("correct") == "true"){
-            answers[i].getElementsByTagName("m")[0].setAttribute("style", "display");
-           answers[i].getElementsByTagName("m")[0].innerHTML = " &#10004";
-           answers[i].style.color = "green";
-        }
         }
     if (incorAns == 0 && corAns == 0) {
          questions[currentPos].getElementsByTagName("message")[0].innerHTML = "Не вибрано жодного варіанту.";
@@ -118,7 +104,6 @@ while (currentPos < questions.length) {
         if (questions[currentPos].getAttribute("type") == "Multiple"){createMultiple(currentPos);}
         if (questions[currentPos].getAttribute("type") == "CheckBox"){createCheckBox(currentPos);}
         questions[currentPos].innerHTML += "<button onclick='findCorrect(" + currentPos + ")'>Перевірити</button>";
-      //  questions[currentPos].innerHTML += "<input type='button' onclick='showAns(" + currentPos + ")' style='display:none' value='Показати відповідь' id='s" + currentPos + "'/>";
         currentPos++;
   }
 }
