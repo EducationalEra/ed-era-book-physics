4fe353f57da5ede5c20ab78c5cedd23b086b7d84
madvolume <gleb.generalyuk@ed-era.com>
2015-06-25 12:34:53 +0300

    Update custom/js/popup.js

--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,8 +1,3 @@
- $(document).ready(function(){
-        
-    $( "div.popup" ).replaceWith( "<div class='" + "boxCourse" + "'><div class='" + "textBlock" +"'>Ще більше відео матеріалів в <eo>онлайн-курсі</eo> від розробників цієї книги</div><a href='" + "https://study.ed-era.com/courses/EdEra/P101/p101/about?_ga=1.67151697.1581692993.1410539408" + "'><button class='" + "but2" + "'>Перейти на курс</button></a><img src='" + "https://rawgit.com/chudaol/ed-era-book-physics/master/custom/css/close.png'" + "class='" + "closeBut" + "'/></div>" );    
-        
-    $(".closeBut").click(function(){
-        $(".boxCourse").hide();
-    });
-    });
+$(document).ready(function(){
+   $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відео матеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/P101/p101/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/P101/p101/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
+   });
