6a15690fbb6d4aef02de249a4e2d15a5d379ea64
chudaol <chudaol@gmail.com>
2017-09-23 17:33:25 +0000

    Updates custom/js/custom.js
    Auto commit by GitBook Editor

--- custom/js/custom.js
+++ custom/js/custom.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+  function startMathJax () {
+    MathJax.Hub.Config({
+      tex2jax: {}
+    });
+
+    gitbook.events.bind("page.change", function() {
+      MathJax.Hub.Typeset()
+    });
+  }
+  gitbook.events.bind("start", function () {
+    var interval = null
+    interval = setInterval(function () {
+      if (typeof MathJax === "object") {
+        startMathJax()
+        clearInterval(interval)
+      }
+    }, 1000)
+  })
+
   function init () {
     var href, $lastLink;
 
